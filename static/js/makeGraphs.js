c61a6c46fda2437053b54f4d792f039a081c9fd7
Mark Piraino <m.piraino3@gmail.com>
2020-10-10 10:50:44 -0500

    starter code for line graph

--- static/js/makeGraphs.js
+++ static/js/makeGraphs.js
@@ -1,5 +1,8 @@
+//neighborhoos dropdown list
+//output nbhd
+
 // pulls data from API and runs the functions to make the graphs
-d3.json("/api/v1/lincoln+park").then(function(data){
+d3.json(`/api/v1/${nbhd}`).then(function(data){
  buildLineGraph(data);
  //other graphs
 });
