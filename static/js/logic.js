4865c364eddf859eeb05af1583956f8cd01ed10f
unknown <shakena.e.jackson@gmail.com>
2020-10-12 20:21:57 -0500

    updated map code

--- static/js/logic.js
+++ static/js/logic.js
@@ -19,58 +19,46 @@ var myMap = L.map("overViewMap", {
   // Use this link to get the geojson data.
 var link = "data/Overview_price_age.geojson";
 
-// // Grabbing our GeoJSON data..
-// d3.json(link, function(data) {
-//   // Creating a GeoJSON layer with the retrieved data
-//   L.geoJson(data).addTo(myMap);
-// });
+// Grabbing our GeoJSON data..
+d3.json(link, function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  L.geoJson(data).addTo(myMap);
+});
 
-  
-// //   // Grab data with d3
-  d3.json(link, function(data) {
-  
-    // Create a new choropleth layer
-    geojson = L.choropleth(data, {
-  
-      // Define what  property in the features to use
-      valueProperty: "Sale_Price",
-  
-      // Set color scale
-      scale: ["#ffffb2", "#b10026"],
-  
-      // Binding a pop-up to each layer
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup("Neighborhood: " + feature.properties.coordinates + "<br>Sale Price:<br>" +
-          "$" + feature.properties.avg_price);
-    //   }
-    // // }).addTo(myMap);
-  
-    // Set up the legend
-    var legend = L.control({ position: "bottomright" });
-    legend.onAdd = function() {
-      var div = L.DomUtil.create("div", "info legend");
-      var limits = geojson.options.limits;
-      var colors = geojson.options.colors;
-      var labels = [];
-  
-      // Add min & max
-      var legendInfo = "<h1>Avg_Price</h1>" +
-        "<div class=\"labels\">" +
-          "<div class=\"min\">" + limits[0] + "</div>" +
-          "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-        "</div>";
-  
-      div.innerHTML = legendInfo;
-  
-      limits.forEach(function(limit, index) {
-        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-      });
-  
-      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-      return div;
-    };
-  
-    // Adding legend to the map
-    ledgend.addTo(myMap);
-   
-  });
+
+
+  var choroplethLayer = L.choropleth(geojson, {
+    valueProperty: 'Ave_Price',
+    scale: ['orange', 'red'],
+    steps: 5,
+    mode: 'q',
+    style: {
+      color: '#fff',
+      weight: 2,
+      fillOpacity: 0.8
+    },
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup('Neighborhood ' + feature.properties.coordinates + '<br>' + feature.properties.ave_price)
+    }
+  }).addTo(map)
+
+  // Add legend (don't forget to add the CSS from index.html)
+  var legend = L.control({ position: 'bottomright' })
+  legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend')
+    var limits = choroplethLayer.options.limits
+    var colors = choroplethLayer.options.colors
+    var labels = []
+
+    // Add min & max
+    div.innerHTML = '<div class="labels"><div class="min">' + limits[0] + '</div> \
+			<div class="max">' + limits[limits.length - 1] + '</div></div>'
+
+    limits.forEach(function (limit, index) {
+      labels.push('<li style="background-color: ' + colors[index] + '"></li>')
+    })
+
+    div.innerHTML += '<ul>' + labels.join('') + '</ul>'
+    return div
+  }
+  legend.addTo(map);
