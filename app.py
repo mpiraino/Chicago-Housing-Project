ff1b79e2a17009435348479e992888e90c5f0b32
Mark Piraino <m.piraino3@gmail.com>
2020-10-12 19:30:19 -0500

    serving geojson in all neighborhood on the api

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ from sqlalchemy import create_engine, inspect, func
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 import pandas as pd
+import json
+
+
 
 # create instance of Flask app
 app = Flask(__name__)
-
 
 # create route that renders index.html template
 @app.route("/")
@@ -27,13 +29,9 @@ def api(neighborhood):
     nbhd = neighborhood.replace("+", " ").upper()
 
     if nbhd == "ALL":
-        data = pd.read_sql("select Sale_Price, Age, nb from housing inner join tract_to_nb on housing.Census_Tract = tract_to_nb.tract;", session.bind)
-        processed_data = data.loc[data['Sale_Price']>1]
-        overview = processed_data.groupby('nb').agg({
-                                        'Sale_Price':  'mean',
-                                        'Age':'mean'
-                                        })
-        return jsonify(overview.to_dict())
+        with open('static/geo_data/Overview_price_age.json', 'r') as f:
+            data = json.load(f)
+        return data
     else:
         data = pd.read_sql(f"select Longitude, Latitude, Sale_Date, Sale_Price, Sale_Year, Age, Address from housing inner join tract_to_nb on housing.Census_Tract = tract_to_nb.tract where tract_to_nb.nb = '{nbhd}';", session.bind).dropna()
         processed_data = data.loc[data['Sale_Price']>1]
